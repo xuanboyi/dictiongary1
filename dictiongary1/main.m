309d6cd165654e6cf73bd8b3af4c9b2b45633d68
玄博屹 <genhiroshi@xuanboyideMacBook-Pro.local>
2017-12-13 18:16:27 +0800

    Initial Commit

---
+++ dictiongary1/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  dictiongary1
+//
+//  Created by 玄博屹 on 2017/12/13.
+//  Copyright © 2017年 玄博屹. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
